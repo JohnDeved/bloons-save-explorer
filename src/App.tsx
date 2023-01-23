42eeccb9cdafb43fc76be3282c4efe2273597701
undefined <johann@objekt.stream>
2023-01-23 09:53:42 +0000

    + pack

--- src/App.tsx
+++ src/App.tsx
@@ -4,43 +4,33 @@ import './App.css'
 import { fileOpen } from 'browser-fs-access'
 import { deflate, inflate } from 'pako'
 
-const BLOONS_DUMMY_HEADER_LENGTH = 44;
-const BLOONS_SALT_LENGTH = 24;
-const BLOONS_KEY_LENGTH = 16;
-const BLOONS_IV_LENGTH = 16;
-const BLOONS_DERIVE_ITERATIONS = 10;
-const BLOONS_PASSWORD_INDEX = 2;
-const BLOONS_COMPRESSION_LEVEL = 3;
-const BLOONS_COMPRESSION_LOG2_BUFFER_SIZE = 11; // 2^11 == 2048
-
 async function unpack(input: ArrayBuffer, password = '11') {
   // open input file
   const inputData = new Uint8Array(input)
 
   // read dummy header (44 bytes of garbage?)
-  const dummyHeader = inputData.slice(0, BLOONS_DUMMY_HEADER_LENGTH)
+  const dummyHeader = inputData.slice(0, 44)
+  console.log('dummyHeader', dummyHeader)
 
   // read password index; not needed for Bloons TD 6
   const passwordIndex = inputData.slice(dummyHeader.length, dummyHeader.length + 8)
+  console.log('passwordIndex', passwordIndex)
 
   // read salt
-  const salt = inputData.slice(dummyHeader.length + passwordIndex.length, dummyHeader.length + passwordIndex.length + BLOONS_SALT_LENGTH)
+  const salt = inputData.slice(dummyHeader.length + passwordIndex.length, dummyHeader.length + passwordIndex.length + 24)
+
+  console.log('header length', dummyHeader.length + passwordIndex.length + salt.length)
 
   // derive key and iv from salt and password
   const raw = await crypto.subtle.deriveBits(
-    { name: 'PBKDF2', salt, iterations: BLOONS_DERIVE_ITERATIONS, hash: 'SHA-1' },
+    { name: 'PBKDF2', salt, iterations: 10, hash: 'SHA-1' },
     await crypto.subtle.importKey('raw', new TextEncoder().encode(password), 'PBKDF2', false, ['deriveBits']),
-    BLOONS_KEY_LENGTH * 8 + BLOONS_IV_LENGTH * 8
+    256
   )
 
   // set key and iv
   const iv = raw.slice(0, 16)
   const key = raw.slice(-16)
-
-  // log key and iv as base64
-  console.log('key', btoa(String.fromCharCode(...new Uint8Array(key))))
-  console.log('iv', btoa(String.fromCharCode(...new Uint8Array(iv))))
-  console.log({raw, key, iv, inputData, dummyHeader, passwordIndex, salt, input})
 
   // decrypt data
   const decryptedData = await crypto.subtle.decrypt(
@@ -49,22 +39,53 @@ async function unpack(input: ArrayBuffer, password = '11') {
     inputData.slice(dummyHeader.length + passwordIndex.length + salt.length)
   )
 
-  console.log({decryptedData})
-  console.log('decryptedData', btoa(String.fromCharCode(...new Uint8Array(decryptedData))))
   // // decompress data
   const decompressedData = inflate(decryptedData)
   const dataString = new TextDecoder().decode(decompressedData)
-  const dataJson = JSON.parse(dataString)
-  console.log({ dataJson })
+  const data = JSON.parse(dataString)
+  return { data, dataString, iv, key, salt, dummyHeader, passwordIndex }
+}
+
+async function pack (input: string, iv: ArrayBuffer, key: ArrayBuffer, salt: Uint8Array, dummyHeader: Uint8Array, passwordIndex: Uint8Array, password = '11') {
+  // compress data
+  const compressedData = deflate(input, {level: 3})
+
+  // encrypt data
+  const encryptedData = await crypto.subtle.encrypt(
+    { name: 'AES-CBC', iv },
+    await crypto.subtle.importKey('raw', key, 'AES-CBC', false, ['encrypt', 'decrypt']),
+    compressedData
+  )
+
+  // write salt
+  const saltData = new Uint8Array(salt)
+
+  // write data
+  const data = new Uint8Array(encryptedData)
+
+  console.log('header length', dummyHeader.length + passwordIndex.length + saltData.length)
+
+  // concatenate all data
+  const output = new Uint8Array(dummyHeader.length + passwordIndex.length + saltData.length + data.length)
+  output.set(dummyHeader)
+  output.set(passwordIndex, dummyHeader.length)
+  output.set(saltData, dummyHeader.length + passwordIndex.length)
+  output.set(data, dummyHeader.length + passwordIndex.length + saltData.length)
+
+  return output.buffer
 }
 
 function App() {
   const handleClick = useCallback(async () => {
     const file = await fileOpen()
     const buffer = await file.arrayBuffer()
-    console.log(file,buffer)
-    const data = await unpack(buffer).catch(console.error)
-    console.log(data)
+    const data = await unpack(buffer)
+    // console.log(data)
+    const packed = await pack(data.dataString, data.iv, data.key, data.salt, data.dummyHeader, data.passwordIndex)
+    console.log({packed, buffer})
+    // log as base64
+    // console.log(btoa(new Uint8Array(packed).reduce((data, byte) => data + String.fromCharCode(byte), '')))
+    // console.log(btoa(new Uint8Array(buffer).reduce((data, byte) => data + String.fromCharCode(byte), '')))
   }, [])
 
   return (
